5c6849e4b5787b18cc1b07cb3496c911afec09f3
nsfzebras <nsfzebras@gmail.com>
2014-09-21 21:06:10 -0400

    Creates initial filtering functionality.

---
+++ src/reddit_filter.js
@@ -0,0 +1,30 @@
+javascript:(function() {
+
+  /*  
+      AUTHOR:       NSF_Zebras 
+
+      Version:      1.0
+
+      Description:  Filter a reddit page by content-type.
+
+   */
+
+  var globalVersion = "v1.0 - NSF_Zebras" ;
+
+  function main() { 
+    
+    jQuery( "div#siteTable p.title a:not(span a)" ).each( function( index, item ) { 
+
+      var href = jQuery( item ).attr( "href" ).toLowerCase() ;
+      
+      if( href.indexOf( ".gif" ) == -1 && href.indexOf( "gfycat" ) == -1 ) { 
+        jQuery( item ).parent().parent().parent().hide()
+      }
+
+    })  ;   
+
+  }
+
+  main() ; 
+
+})();
